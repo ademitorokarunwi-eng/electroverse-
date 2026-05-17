064f947ff2be83c9edbe0816823663f5cf757992
ademitorokarunwi-eng <ademitorokarunwi@gmail.com>
2026-05-17 20:40:23 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -202,5 +202,7 @@ def get_location(reference):
     return jsonify(result)
 
 
-if __name__ == "__main__":
+if_name__ == "__main__":
+    create_tables()
+    import_integrated_data()
     app.run(debug=True)
